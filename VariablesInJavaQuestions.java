702412c09a47be0d4e7762c87a10e9902fc6ec2a
theBloodpal <67363517+theBloodpal@users.noreply.github.com>
2022-10-25 20:15:43 +0530

    Create VariablesInJavaQuestions.java

---
+++ VariablesInJavaQuestions.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Questions{
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        //Question 1 - Average of three numbers
+
+        // double a = sc.nextInt();
+        // double b = sc.nextInt();
+        // double c = sc.nextInt();
+        // double d = (a+b+c)/3 ;
+        // System.out.println("Average of these 3 numbers is : " + d);
+
+        //Question 2 - Area of square
+
+        // System.out.print("Enter the size : ");
+        // double a = sc.nextInt();
+        // double b = a*a;
+        // System.out.println("The area of square is : " + b);
+
+        // Question 3 - Bill with GST
+        // System.out.print("Enter the cost for a pencil : ");
+        // float a = sc.nextFloat();
+        // System.out.print("Enter the cost for a pen : ");
+        // float b = sc.nextFloat();
+        // System.out.print("Enter the cost for an eraser : ");
+        // float c = sc.nextFloat();
+        // System.out.println("The cost of a pensil :" + a);
+        // System.out.println("The cost of a pen :" + b);
+        // System.out.println("The cost of an eraser:" + c);
+        // float d = a+b+c;
+        // System.out.println("The total cost of all the items is : " + d + " excluding GST.");
+        // float p = d * 0.18f ;
+        // float g = d + p;
+        // System.out.println("But with 18% GST the total cost of the items is : " + g);
+
+    }
+}
